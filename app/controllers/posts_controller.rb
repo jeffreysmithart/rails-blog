5f650c9cfe917b75faaa38faded4a95d53c774c6
Jeffrey Smith <jeffreysmithart@gmail.com>
2015-05-25 10:27:07 -0500

    Add validation to new post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,19 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-		flash[:notice] = "Post successfully created"
-
-		redirect_to @post
+		
+		if @post.save
+			flash[:notice] = "Post successfully created"
+			redirect_to @post
+		else
+			render 'new'
+		end
+		
 	end
 
 	def show 
